cf945a38291dd6e854ce48d4f4386375b0850e25
liulihao <551772063@qq.com>
2024-09-20 10:42:54 +0800

    feat: 你干嘛

--- src/views/test/t3.vue
+++ src/views/test/t3.vue
@@ -1,15 +1,6 @@
 <script setup lang="ts">
 import { ref, getCurrentInstance } from 'vue'
 const { proxy } = getCurrentInstance()
-import { ElNotification } from 'element-plus'
-
-const num = ref(0)
-setInterval(() => {
-  ElNotification({
-    message: `正在上传, 进度${num.value++}%`,
-    duration: 0,
-  })
-}, 1000)
 </script>
 
 <template>
